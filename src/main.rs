d4ef8b9b5e1fc739db910a6690fa001eb96f3667
Piotr Wittchen <piotr@wittchen.io>
2025-03-05 23:24:50 +0100

    simplified and refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
 use std::fs;
-use swc_common::errors::{ColorConfig, Handler};
-use swc_common::{sync::Lrc, BytePos, SourceMap};
-use swc_ecma_parser::{lexer::Lexer, StringInput, Syntax};
+use std::rc::Rc;
+use swc_common::{BytePos, SourceFile, SourceMap, sync::Lrc};
+use swc_ecma_parser::{StringInput, Syntax, lexer::Lexer};
 
 #[derive(clap::Parser)]
 #[command(name = "JSVS")]
@@ -16,21 +16,31 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
     let filepath = cli.filepath;
+
     if fs::metadata(&filepath).is_err() {
         eprintln!("Error: filepath {} does not exist", &filepath);
         return;
     }
 
     let file_content = fs::read_to_string(&filepath).expect("Cannot read file");
+    let source_map: Lrc<SourceMap> = Default::default();
 
-    let cm: Lrc<SourceMap> = Default::default();
-
-    let source_file = cm.new_source_file(
+    let source_file = source_map.new_source_file(
         swc_common::FileName::Custom((&filepath).as_str().into()).into(),
         file_content.clone(),
     );
 
-    let lexer = Lexer::new(
+    let lexer = create_lexer(&source_file);
+    let mut parser = swc_ecma_parser::Parser::new_from(lexer);
+
+    match parser.parse_script() {
+        Ok(script) => println!("Parsed successfully: {:?}", script),
+        Err(_) => eprintln!("Parsing failed!"),
+    }
+}
+
+fn create_lexer(source_file: &Rc<SourceFile>) -> Lexer {
+    Lexer::new(
         Syntax::Es(Default::default()),
         Default::default(),
         StringInput::new(
@@ -39,16 +49,5 @@ fn main() {
             BytePos(source_file.src.len() as u32),
         ),
         None,
-    );
-
-    let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm));
-    let mut parser = swc_ecma_parser::Parser::new_from(lexer);
-
-    match parser.parse_script() {
-        Ok(script) => println!("Parsed successfully: {:?}", script),
-        Err(err) => {
-            err.into_diagnostic(&handler).emit();
-            eprintln!("Parsing failed!");
-        }
-    }
+    )
 }

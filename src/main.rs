5f02abc1149dfaa6866233553733282415c5c7cc
Piotr Wittchen <piotr@wittchen.io>
2025-03-05 21:30:45 +0100

    added CLI with clap dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::{Parser};
+#[derive(Parser)]
+#[command(name = "JSVS")]
+#[command(version = "0.1.0")]
+#[command(about = "JavaScript Vulnerability Scanner", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    filepath: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("filename: {}", cli.filepath);
 }

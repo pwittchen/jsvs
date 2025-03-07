8855897deaa2b619c085990d5f890cd91db76959
Piotr Wittchen <piotr@wittchen.io>
2025-03-07 23:48:36 +0100

    heavy project refactoring: added VulnerabilityRule and DetectedVulnerability structs and VulnerabilityType enum, code was split into smaller functions and re-organized

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use base64::{Engine as _, engine::general_purpose};
+use crate::VulnerabilityType::{Alert, Warning};
+use base64::{engine::general_purpose, Engine as _};
 use clap::Parser;
+use colored::Colorize;
 use regex::Regex;
 use std::fs;
 
@@ -12,6 +14,25 @@ struct Cli {
     filepath: String,
 }
 
+struct VulnerabilityRule {
+    keywords: Vec<String>,
+    description: String,
+    vulnerability_type: VulnerabilityType,
+}
+
+struct DetectedVulnerability {
+    keyword: String,
+    keyword_index: usize,
+    description: String,
+    vulnerability_type: VulnerabilityType,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum VulnerabilityType {
+    Warning,
+    Alert,
+}
+
 fn main() {
     let cli = Cli::parse();
     let filepath = cli.filepath;
@@ -22,103 +43,281 @@ fn main() {
     }
 
     let file_content = fs::read_to_string(filepath).expect("Cannot read file");
-    analyze_javascript(file_content);
+    analyze_javascript(file_content, false);
 }
 
-fn analyze_javascript(file_content: String) {
-    //TODO: return vector with found issues instead of printing them directly here
-    //TODO: define types of vector: warning, alert
-    //TODO: check difference between indexes for xhr.open/XMLHttpReq.responseText and eval/execscript
-    // where low value is possibility of remote script execution
-    //TODO: when all basic pattern detection will be implemented, refactor this code
+fn analyze_javascript(file_content: String, is_base64_decoded: bool) {
+    let mut detected_vulnerabilities: Vec<DetectedVulnerability> = Vec::new();
 
-    let suspicious_keywords = [
-        "eval",
-        "execscript",
-        "document.write",
-        "xmlhttprequest",
-        "xhr.open",
-        "xmlhttpreq.responsetext",
-        "atob",
-        "btoa",
-        "addeventlistener(\"keydown\"",
-        "addeventlistener(\"keyup\"",
-        "addeventlistener(\"keypress\"",
-        "addeventlistener(\"submit\"",
-        "addeventlistener(\"load\"",
-        "addeventlistener(\"unload\"",
-        "addeventlistener(\"beforeunload\"",
-        "addeventlistener",
-        "document.queryselectorall",
-        "preventdefault()",
-        "formdata()",
-        "fetch",
-        "localstorage",
-        "setitem",
-        "getitem",
-        "removeitem",
-        "document.createelement(\"script\")",
-        "document.createelement(\"iframe\")",
-        "document.createelement",
-        "iframe",
-        "http://",
-        "https://",
-        "customerData",
-        ".js",
-        "input-payment",
-        "input-payment-firstname",
-        "input-payment-lastname",
-        "input-payment-email",
-        "input-payment-telephone",
-        "input-payment-city",
-        "input-payment-postcode",
-        "input-firstname",
-        "input-lastname",
-        "input-cc-owner",
-        "input-cc-number",
-        "input-cc-expire-date",
-        "cc_expire_date_year",
-        "input-cc-cvv2",
-        "holder",
-        "cvv",
-        "email",
-        "telephone",
-        "phone",
-        "address",
-        "city",
-        "postcode",
-        "zip",
-        "zone",
-        "state",
-        "country",
-    ];
-    for keyword in &suspicious_keywords {
-        if let Some(index) = &file_content.to_lowercase().find(keyword) {
-            println!("keyword: {} found at index: {}", keyword, index);
-        }
+    for v in find_vulnerabilities_by_rules(&file_content, is_base64_decoded) {
+        detected_vulnerabilities.push(v);
     }
 
-    let mut hex_counter: usize = 0;
-    let hex_pattern = r"(?i)\b(?:0x[a-f0-9]+|#[a-f0-9]{6}|\b[a-f0-9]{8}\b)\b";
-    let re = Regex::new(hex_pattern).unwrap();
-    for _ in re.find_iter(&file_content) {
-        hex_counter += 1;
-    }
-    println!("Found {} hex values", hex_counter);
-
-    let base64_pattern = r"(?i)\b[A-Za-z0-9+/=]{50,}\b";
-    let re = Regex::new(base64_pattern).unwrap();
-    let mut base64_encoded_text = String::new();
-    for value in re.find_iter(&file_content) {
-        base64_encoded_text.push_str(&value.as_str());
+    for v in find_vulnerabilities_by_hex_count(&file_content) {
+        detected_vulnerabilities.push(v);
     }
 
+    for v in find_vulnerabilities_in_base64(&file_content) {
+        detected_vulnerabilities.push(v);
+    }
+
+    print_detected_vulnerabilities(&mut detected_vulnerabilities);
+}
+
+fn find_vulnerabilities_in_base64(content: &String) -> Vec<DetectedVulnerability> {
+    let base64_encoded_text = encode_base64_text(content);
+    let mut detected_vulnerabilities: Vec<DetectedVulnerability> = Vec::new();
     if !base64_encoded_text.as_str().is_empty() {
+        detected_vulnerabilities.push(DetectedVulnerability {
+            keyword: "base64".to_string(),
+            keyword_index: 0,
+            description: String::from(
+                "Found base64 encoded text what may suggest hiding some information",
+            ),
+            vulnerability_type: Warning,
+        });
+
         let decoded_bytes = general_purpose::STANDARD_NO_PAD
             .decode(base64_encoded_text)
             .expect("Failed to decode base64 string");
 
         let base64_decoded_text = String::from_utf8(decoded_bytes).expect("Invalid UTF-8");
-        analyze_javascript(base64_decoded_text);
+        analyze_javascript(base64_decoded_text, true);
     }
+    detected_vulnerabilities
+}
+
+fn encode_base64_text(content: &String) -> String {
+    let base64_pattern = r"(?i)\b[A-Za-z0-9+/=]{50,}\b";
+    let re = Regex::new(base64_pattern).unwrap();
+    let mut base64_encoded_text = String::new();
+    for value in re.find_iter(&content) {
+        base64_encoded_text.push_str(&value.as_str());
+    }
+    base64_encoded_text
+}
+
+fn count_hex_values(content: &String) -> usize {
+    let mut hex_counter: usize = 0;
+    let hex_pattern = r"(?i)\b(?:0x[a-f0-9]+|#[a-f0-9]{6}|\b[a-f0-9]{8}\b)\b";
+    let re = Regex::new(hex_pattern).unwrap();
+    for _ in re.find_iter(&content) {
+        hex_counter += 1;
+    }
+    hex_counter
+}
+
+fn find_vulnerabilities_by_hex_count(content: &String) -> Vec<DetectedVulnerability> {
+    let hex_counter = count_hex_values(&content);
+    let mut detected_vulnerabilities: Vec<DetectedVulnerability> = Vec::new();
+
+    if hex_counter > 0 {
+        detected_vulnerabilities.push(DetectedVulnerability {
+            keyword: "hex".to_string(),
+            keyword_index: 0,
+            description: if hex_counter < 10 {
+                String::from(format!("{} hexadecimal values", hex_counter))
+            } else {
+                String::from(format!(
+                    "{} hexadecimal values suggests heavy obfuscation what can hide malware",
+                    hex_counter
+                ))
+            },
+            vulnerability_type: if hex_counter < 10 { Warning } else { Alert },
+        });
+    }
+
+    detected_vulnerabilities
+}
+
+fn find_vulnerabilities_by_rules(
+    file_content: &String,
+    is_base64_decoded: bool,
+) -> Vec<DetectedVulnerability> {
+    let mut detected_vulnerabilities: Vec<DetectedVulnerability> = Vec::new();
+    for rule in get_vulnerability_rules() {
+        for keyword in rule.keywords {
+            if let Some(index) = &file_content.to_lowercase().find(&keyword) {
+                let description = if is_base64_decoded {
+                    format!("{} (base64 decoded)", &rule.description.clone())
+                } else {
+                    rule.description.to_string()
+                };
+
+                let vulnerability_type = if rule.vulnerability_type == Alert {
+                    Alert
+                } else if is_base64_decoded {
+                    Alert
+                } else {
+                    Warning
+                };
+
+                detected_vulnerabilities.push(DetectedVulnerability {
+                    keyword,
+                    keyword_index: *index,
+                    description: description.to_string(),
+                    vulnerability_type,
+                });
+            }
+        }
+    }
+    detected_vulnerabilities
+}
+
+fn print_detected_vulnerabilities(detected_vulnerabilities: &mut Vec<DetectedVulnerability>) {
+    for v in detected_vulnerabilities {
+        println!(
+            " • {}\t index: {}\t keyword: {} {}",
+            if v.vulnerability_type == Alert {
+                "Alert".red()
+            } else {
+                "Warning".yellow()
+            },
+            v.keyword_index.to_string().blue(),
+            v.keyword.to_string().underline(),
+            v.description.cyan()
+        );
+    }
+}
+
+fn get_vulnerability_rules() -> [VulnerabilityRule; 16] {
+    [
+        VulnerabilityRule {
+            keywords: vec![
+                String::from("eval"),
+                String::from("execscript"),
+                String::from("document.write"),
+                String::from("document.createelement(\"script\")"),
+            ],
+            description: String::from("Possible XSS"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![
+                String::from("xmlhttprequest"),
+                String::from("xhr.open"),
+                String::from("xmlhttpreq.responsetext"),
+                String::from("fetch"),
+                String::from("https://"),
+            ],
+            description: String::from("Possible insecure API call"),
+            vulnerability_type: Warning,
+        },
+        VulnerabilityRule {
+            keywords: vec![String::from("atob"), String::from("btoa")],
+            description: String::from("Suspicious Base64 encoding or decoding"),
+            vulnerability_type: Warning,
+        },
+        VulnerabilityRule {
+            keywords: vec![
+                String::from("addeventlistener(\"keydown\""),
+                String::from("addeventlistener(\"keyup\""),
+                String::from("addeventlistener(\"keypress\""),
+            ],
+            description: String::from("Possible keylogger"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![
+                String::from("addeventlistener"),
+                String::from("addeventlistener(\"submit\""),
+                String::from("addeventlistener(\"load\""),
+                String::from("addeventlistener(\"unload\""),
+                String::from("addeventlistener(\"beforeunload\""),
+            ],
+            description: String::from("Triggering actions on event"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![
+                String::from("document.queryselectorall"),
+                String::from("formdata()"),
+            ],
+            description: String::from("Possible user data exfiltration"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![String::from("preventdefault()")],
+            description: String::from("Changing app behavior by preventing default actions"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![
+                String::from("localstorage"),
+                String::from("sessionstorage"),
+                String::from("getitem"),
+            ],
+            description: String::from("Reading local storage or session storage capabilities"),
+            vulnerability_type: Warning,
+        },
+        VulnerabilityRule {
+            keywords: vec![String::from("setitem"), String::from("removeitem")],
+            description: String::from("local storage or session storage manipulation"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![String::from("createlement")],
+            description: String::from("DOM manipulation"),
+            vulnerability_type: Warning,
+        },
+        VulnerabilityRule {
+            keywords: vec![String::from("document.createelement(\"iframe\")")],
+            description: String::from("IFrame injection with possible clickjacking"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![String::from("iframe")],
+            description: String::from("IFrame found, what can enable hidden tracking"),
+            vulnerability_type: Warning,
+        },
+        VulnerabilityRule {
+            keywords: vec![String::from(".js")],
+            description: String::from("External script usage"),
+            vulnerability_type: Warning,
+        },
+        VulnerabilityRule {
+            keywords: vec![String::from("http://")],
+            description: String::from("Insecure API Call without SSL"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![
+                String::from("customerData"),
+                String::from("input-firstname"),
+                String::from("input-lastname"),
+                String::from("email"),
+                String::from("telephone"),
+                String::from("phone"),
+                String::from("address"),
+                String::from("city"),
+                String::from("postcode"),
+                String::from("zip"),
+                String::from("zone"),
+                String::from("country"),
+            ],
+            description: String::from("Possible user data exfiltration with specified fields"),
+            vulnerability_type: Alert,
+        },
+        VulnerabilityRule {
+            keywords: vec![
+                String::from("input-payment"),
+                String::from("input-payment-firstname"),
+                String::from("input-payment-lastname"),
+                String::from("input-payment-email"),
+                String::from("input-payment-telephone"),
+                String::from("input-payment-city"),
+                String::from("input-payment-postcode"),
+                String::from("input-cc-owner"),
+                String::from("input-cc-number"),
+                String::from("input-cc-expire-date"),
+                String::from("input-cc-cvv2"),
+                String::from("holder"),
+                String::from("cvv"),
+                String::from("ccv"),
+                String::from("cc_expire_date_year"),
+            ],
+            description: String::from("Possible user payment data exfiltration and money stealing"),
+            vulnerability_type: Alert,
+        },
+    ]
 }

6b79efc9c2270b4ee36f2d2cb913e659c70031f7
Piotr Wittchen <piotr@wittchen.io>
2025-03-08 01:33:34 +0100

    updated summary formatting and added vulnerabilities counting

--- src/main.rs
+++ src/main.rs
@@ -43,10 +43,50 @@ fn main() {
     }
 
     let file_content = fs::read_to_string(filepath).expect("Cannot read file");
-    analyze_javascript_code(file_content, false);
+    let vulnerabilities = find_vulnerabilities_in_javascript_code(file_content, false);
+    print_summary_of_the_analysis(&vulnerabilities);
 }
 
-fn analyze_javascript_code(file_content: String, is_base64_decoded: bool) {
+fn print_summary_of_the_analysis(vulnerabilities: &Vec<DetectedVulnerability>) {
+    println!();
+    println!(" ■ JavaScript Vulnerability Detection Summary");
+    println!();
+
+    if !vulnerabilities.is_empty() {
+        print_detected_vulnerabilities(&vulnerabilities);
+    } else {
+        println!("{}", " ✓ Looks like this code is safe!".bright_green());
+    }
+
+    println!();
+    println!(
+        "{} {} {}",
+        " ✕ Detected",
+        vulnerabilities.len().to_string().red(),
+        "vulnerabilities"
+    );
+}
+
+fn print_detected_vulnerabilities(detected_vulnerabilities: &Vec<DetectedVulnerability>) {
+    for v in detected_vulnerabilities {
+        println!(
+            " • {}\t index: {}\t keyword: {} {}",
+            if v.vulnerability_type == Alert {
+                "Alert".red()
+            } else {
+                "Warning".yellow()
+            },
+            v.keyword_index.to_string().bright_blue(),
+            v.keyword.to_string().underline(),
+            v.description.cyan()
+        );
+    }
+}
+
+fn find_vulnerabilities_in_javascript_code(
+    file_content: String,
+    is_base64_decoded: bool,
+) -> Vec<DetectedVulnerability> {
     let mut vulnerabilities: Vec<DetectedVulnerability> = Vec::new();
 
     vulnerabilities.extend(find_vulnerabilities_by_rules(
@@ -56,8 +96,7 @@ fn analyze_javascript_code(file_content: String, is_base64_decoded: bool) {
     vulnerabilities.extend(find_suspocious_hex_obfuscation(&file_content));
     vulnerabilities.extend(find_vulnerabilities_in_base64(&file_content));
     vulnerabilities.extend(find_possible_remote_code_execution(&vulnerabilities));
-
-    print_detected_vulnerabilities(vulnerabilities);
+    vulnerabilities
 }
 
 fn find_vulnerabilities_by_rules(
@@ -116,7 +155,10 @@ fn find_vulnerabilities_in_base64(content: &String) -> Vec<DetectedVulnerability
             .expect("Failed to decode base64 string");
 
         let base64_decoded_text = String::from_utf8(decoded_bytes).expect("Invalid UTF-8");
-        analyze_javascript_code(base64_decoded_text, true);
+        detected_vulnerabilities.extend(find_vulnerabilities_in_javascript_code(
+            base64_decoded_text,
+            true,
+        ));
     }
     detected_vulnerabilities
 }
@@ -206,22 +248,6 @@ fn find_possible_remote_code_execution(
         }
     }
     detected_vulnerabilities
-}
-
-fn print_detected_vulnerabilities(detected_vulnerabilities: Vec<DetectedVulnerability>) {
-    for v in detected_vulnerabilities {
-        println!(
-            " • {}\t index: {}\t keyword: {} {}",
-            if v.vulnerability_type == Alert {
-                "Alert".red()
-            } else {
-                "Warning".yellow()
-            },
-            v.keyword_index.to_string().bright_blue(),
-            v.keyword.to_string().underline(),
-            v.description.cyan()
-        );
-    }
 }
 
 fn get_vulnerability_rules() -> [VulnerabilityRule; 16] {
